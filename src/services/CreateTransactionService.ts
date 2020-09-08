d2f95203a47c95e149626d9fd779de46e3fdf0d2
Matheus Pissaia <pissaia.matheus03@gmail.com>
2020-09-07 21:29:47 -0300

    Desafio NodeJS

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: TransactionRequest): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Invalid type of transaction');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
